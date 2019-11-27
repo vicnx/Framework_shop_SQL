6d0ff8737f726ddfa46035e5f699cd967aaf3321
Vicente <andanivicente@gmail.com>
2019-11-27 21:46:13 +0100

    Continuar con doble click. Mostrar en un mensaje dialog bonito. Despues añadirlo al read.

--- Framework_shop_SQL/src/test/test1.java
+++ Framework_shop_SQL/src/test/test1.java
@@ -1,12 +1,16 @@
 package test;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -17,10 +21,10 @@ import functions.Functions;
 public class test1 {
 	public static void main(String[] args) throws Exception {
 		// Cfremos una tabla para mostrar el contenido
-		String columnas[] = { "name", "surname", "phone", "DNI", "email", "birthday", "username", "password" };
+		String columnas[] = { "name", "surname", "DNI", "username" };
 		DefaultTableModel modelo = new DefaultTableModel();
 		modelo.setColumnIdentifiers(columnas);
-		JTable tabla = new JTable();
+		JTable tabla = new JTable(modelo);
 		JScrollPane desplazamiento = new JScrollPane(tabla);
 		JFrame F = new JFrame();
 		JButton close = new JButton("Close");
@@ -28,11 +32,12 @@ public class test1 {
 		desplazamiento.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		tabla.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		tabla.setFillsViewportHeight(true);
-		tabla.setModel(modelo);
 		tabla.setDefaultEditor(Object.class, null);
 		F.getContentPane().add(desplazamiento, BorderLayout.NORTH);
 		F.getContentPane().add(close);
 		F.pack();
+		desplazamiento.setPreferredSize(new Dimension(400, 400));
+		Object ter[] = { desplazamiento };
 		// Read
 		String name = "";
 		String surname = "";
@@ -46,6 +51,41 @@ public class test1 {
 		Connection conn = DataConnection.getConnection();
 		PreparedStatement statement = conn.prepareStatement("SELECT * FROM Admin");
 		ResultSet result = statement.executeQuery();
+
+		tabla.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String name, surname, phone, DNI, email, birthday, username, password, cad = "";
+
+				if (e.getClickCount() == 2) {
+					int row = tabla.getSelectedRow();
+					String dato = (String) tabla.getValueAt(row, 2);
+					try {
+						Connection read = DataConnection.getConnection();
+						PreparedStatement statement2 = read
+								.prepareStatement("SELECT * FROM Admin WHERE DNI='" + dato + "'");
+						ResultSet r = statement2.executeQuery();
+						while (r.next()) {
+							name = r.getString("name");
+							surname = r.getString("surname");
+							phone = r.getString("phone");
+							DNI = r.getNString("DNI");
+							email = r.getString("email");
+							birthday = r.getString("birthday");
+							username = r.getString("username");
+							password = r.getString("password");
+
+							cad = cad + "Name: " + name + "\nSurname: " + surname + "\nPhone: " + phone + "\nDNI: "
+									+ DNI + "\nEmail: " + email + "\nBirthday: " + birthday + "\nusername: " + username
+									+ "\npassword: " + password + "\n\n";
+						}
+					} catch (Exception e2) {
+						// TODO: handle exception
+					} // fintrycatch
+					System.out.println(cad);
+				} // fin 2 cliks
+			}
+		});
+
 		while (result.next()) {
 			name = result.getString("name");
 			surname = result.getString("surname");
@@ -57,7 +97,7 @@ public class test1 {
 			password = result.getString("password");
 
 			// añadimos datos a la tabla
-			modelo.addRow(new Object[] { name, surname, phone, DNI, email, birthday, username, password });
+			modelo.addRow(new Object[] { name, surname, DNI, username });
 			cad = cad + "Name: " + name + "\nSurname: " + surname + "\nPhone: " + phone + "\nDNI: " + DNI + "\nEmail: "
 					+ email + "\nBirthday: " + birthday + "\nusername: " + username + "\npassword: " + password
 					+ "\n\n";
@@ -69,7 +109,9 @@ public class test1 {
 			Functions.mensajeerror("No hay Admins en la BD", "Error");
 
 		} else {
-			F.setVisible(true);
+			JOptionPane.showMessageDialog(null, ter);
+			// F.setVisible(true);
 		}
+
 	}
 }
